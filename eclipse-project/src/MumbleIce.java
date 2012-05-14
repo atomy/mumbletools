13fbfc16a9502bb9b3dcb84c7abc62ba23a1bf4b
Lorentz Roßbacher <lancetekk@googlemail.com>
2012-05-14 14:42:33 +0200

    offizielle stats integriert

--- eclipse-project/src/MumbleIce.java
+++ eclipse-project/src/MumbleIce.java
@@ -1,7 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import Murmur.Channel;
 import Murmur.InvalidSecretException;
@@ -13,6 +21,109 @@ import Murmur.ServerPrx;
 
 
 public class MumbleIce {
+    
+//    public static void main(String[] args) {
+//        
+//        
+//        
+//        int status = 0;
+//        Ice.Communicator ic = null;
+//        ServerPrx serv = null;
+//        ic = Ice.Util.initialize();
+//        Ice.ObjectPrx base = ic.stringToProxy("Meta:tcp -h 127.0.0.1 -p 6502");
+//        MetaPrx meta = MetaPrxHelper.checkedCast(base);
+//        
+//        if (meta == null) {
+//            throw new Error("Invalid proxy");
+//        }
+//        ServerPrx servs[] = null;
+//        
+//        try {
+//            servs = meta.getBootedServers();
+//            Map<String, String> config = meta.getDefaultConf();
+//        } catch (InvalidSecretException e) {
+//            System.err.println("InvalidSecretException! check murmur ice config!");
+//            status = 1;
+//        }
+//        
+//        if((servs != null) && servs.length > 0) {
+//            serv = servs[0];
+//            Map<Integer, Channel> channels = null;
+//            
+//            try {
+//                channels = serv.getChannels(null);
+//            } catch (InvalidSecretException e) {
+//                System.err.println("InvalidSecretException! check murmur ice config!");
+//                status = 1;
+//            } catch (ServerBootedException e) {
+//                System.err.println("Server not booted?");
+//                status = 1;
+//            }
+//            
+//            for(Map.Entry<Integer, Channel> chanEntry : channels.entrySet()) {                  
+//                Channel chan = chanEntry.getValue();
+//                System.out.println("main() chan name: " + chan.name + " chan id: " + chan.id);
+//              
+//                // look for channel with name "..."
+//                if(chan.name.endsWith("--- trolololol ---")) {
+//            
+//                    ArrayList<Integer> subChans = null;
+//                    try {
+//                        subChans = getSubChannelsOfID(serv, chan.id);
+//                    } catch (InvalidSecretException e) {
+//                        System.err.println("InvalidSecretException! check murmur ice config!");
+//                        status = 1;
+//                    } catch (ServerBootedException e) {
+//                        System.err.println("Server not booted?");
+//                        status = 1;
+//                    }     
+//                    
+//                    
+//                    for(Integer i : subChans) {
+//                        
+//                        Channel subChan = null;
+//                        
+//                        try {
+//                            subChan = getChanByID(serv, i);
+//                        } catch (InvalidSecretException e) {
+//                            System.err.println("InvalidSecretException! check murmur ice config!");
+//                            status = 1;
+//                        } catch (ServerBootedException e) {
+//                            System.err.println("Server not booted?");
+//                            status = 1;
+//                        }
+//                        
+//                        if(subChan != null) {
+//                            if(subChan.name.charAt(0) == 'a') {
+//                                subChan.name = "aDOLF";
+//                            }
+//                            String playername = subChan.name.split("\\s+")[0];
+//                            Player player = new Player(playername);
+//                            parseOfficialStats(player);
+//                            subChan.name = player.officialString();
+//                            subChan.description = player.officialString();
+//                            System.out.println(subChan.name);
+//                            serv.setChannelState(subChan);
+//                        }
+//                        
+//                    }
+//                }
+//            }
+//        }
+//        else {
+//            System.err.println("server not found");
+//        }
+//        if (ic != null) {
+//            // Clean up
+//            try {
+//                ic.destroy();
+//            } catch (Exception e) {
+//                System.err.println(e.getMessage());
+//                status = 1;
+//            }
+//        }
+//        System.exit(status);          
+//    }
 
 	/**
 	 * @param args
@@ -39,9 +150,8 @@ public class MumbleIce {
             // debug, printout found servers
             System.out.println("main() found servers: " + servs.length);
             
-            // work with server index 0 if any found
             if(servs.length > 0) {
-            	ServerPrx serv = servs[0];          
+              ServerPrx serv = servs[0];          
             	
             	// get all channels
             	Map<Integer, Channel> channels = serv.getChannels(null);
@@ -59,16 +169,14 @@ public class MumbleIce {
             				// get subchan and work with it
             				Channel subChan = getChanByID(serv, i);
             				if(subChan != null) {
-            					// found a channel starting with "s" (name)
-            					if(subChan.name.charAt(0) == 's') {
-            						// set name so something different
-            						subChan.name = "supidopimo!";
-            					} else if(subChan.name.charAt(0) == 'a') {
-            						subChan.name = "aDOLF";
-            					}
-            					// save channel data structure and apply
-            					serv.setChannelState(subChan);
-            				}
+                                String playername = subChan.name.split("\\s+")[0];
+                                Player player = new Player(playername);
+                                parseOfficialStats(player);
+                                subChan.name = player.officialString();
+                                subChan.description = player.officialString();
+                                System.out.println(subChan.name);
+                                serv.setChannelState(subChan);
+                          }
             			}
             		}
             	}
@@ -119,4 +227,83 @@ public class MumbleIce {
 		}
 		return null;		
 	}
+	
+	/**
+	 * parse playerstats from official database
+	 *
+	 * @param player
+	 */
+	private static void parseOfficialStats(Player player) {
+	    
+	    Pattern p = Pattern.compile("\\d+");
+
+        boolean foundRating = false;
+        boolean foundRank = false;
+        boolean foundWins = false;
+        boolean foundLosses = false;
+        boolean complete = false;
+
+        String rank = "";
+
+        try {
+            URL url = new URL("http://competitive.euw.leagueoflegends.com/ladders/euw/current/rankedsolo5x5?summoner_name=" + URLEncoder.encode(player.getName(), "UTF-8"));
+            InputStream is = url.openStream();
+            InputStreamReader isr = new  InputStreamReader(is);
+            BufferedReader in = new BufferedReader(isr);
+            String zeile = null;
+            
+
+            while ((zeile = in.readLine()) != null) {
+
+                if (foundRating) {
+                    Matcher m = p.matcher(zeile);
+                    if (m.find()) {
+                        player.setOfficialRating(Integer.parseInt(m.group()));
+                    }
+                    foundRating = false;
+                }
+                if (foundWins) {
+                    Matcher m = p.matcher(zeile);
+                    if (m.find()) {
+                        player.setOfficialWins(Integer.parseInt(m.group()));
+                    }
+                    foundWins = false;
+                }
+                if (foundLosses) {
+                    Matcher m = p.matcher(zeile);
+                    if (m.find()) {
+                        player.setOfficialLosses(Integer.parseInt(m.group()));
+                    }
+                    foundLosses = false;
+                }
+                if (foundRank) {
+                    Matcher m = p.matcher(zeile);
+                    while (m.find()) {
+                        rank += m.group();
+                    }
+                    player.setOfficialRank(Integer.parseInt(rank));
+                    foundRank = false;
+                }
+
+                if (!complete) {
+                    if (zeile.contains("<td class=\"views-field views-field-rating views-align-center\" >")) {
+                        complete = true;
+                        foundRating = true;
+                    }
+                    if (zeile.contains("<td class=\"views-field views-field-rank views-align-center\" >")) {
+                        foundRank = true;
+                    }
+                    if (zeile.contains("<td class=\"views-field views-field-wins views-align-center\" >")) {
+                        foundWins = true;
+                    }
+                    if (zeile.contains("<td class=\"views-field views-field-losses views-align-center\" >")) {
+                        foundLosses = true;
+                    }
+                }
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
