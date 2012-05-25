d36322300df18ab5ecb762c4d7909adee8b4ce53
Lorentz Roßbacher <lancetekk@googlemail.com>
2012-05-25 19:46:08 +0200

    wetter eingebunden

--- eclipse-project/src/MumbleIce.java
+++ eclipse-project/src/MumbleIce.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLEncoder;
 import java.util.ArrayList;
@@ -21,8 +22,25 @@ import Murmur.ServerPrx;
 public class MumbleIce implements Runnable{
     
     boolean running = true;
-    int sleeptimer = 3600000;
-    String topchannel = "--- LoL Ranking (EUW) ---";
+    
+    // milliseconds - 1 hour is 3600000
+    int sleeptimer = 10000 / 2;
+    
+    // lolstats yes no
+    boolean lol = true;
+    // name of lolstattopchannel
+    String lolTopchannel = "--- LoL Ranking (EUW) ---";
+    
+    //weather yes no
+    boolean weather = true;
+    // name of weathertopchannel
+    String wetterTopchannel = "--- Wetterkontrollstation ---";
+    
+    //port
+    String port = "6502";
+    
+    //ip
+    String ip = "127.0.0.1";
 
 	/**
 	 * @param args
@@ -132,10 +150,62 @@ public class MumbleIce implements Runnable{
                 }
 
             }
+            isr.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+	
+	
+	private static void getWeather(Location location) {
+	      try {
+	            // Create a URL for the desired page
+	            URL url = new URL("http://thefuckingweather.com/?where="+URLEncoder.encode(location.getPlz(), "UTF-8")+"&unit=c");
+
+	            // Read all the text returned by the server
+	            BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
+	            String line;
+	            while ((line = in.readLine()) != null) {
+	                if (line.contains("<div style=\"float: left;\"><span id=\"locationDisplaySpan\" class=\"small\">")) {
+	                    line = line.replace("<div style=\"float: left;\"><span id=\"locationDisplaySpan\" class=\"small\">", "");
+	                    line = line.replace("</span></div>", "");
+	                    //line = line.trim().split("\\s+")[0];
+	                    location.setLocationName(line.trim().split("\\s+")[0].replace(",", ""));
+	                }
+	                if (line.contains("<p class=\"large\"><span class=\"temperature\" tempf=\"")) {
+	                    line = line.replace("<p class=\"large\"><span class=\"temperature\" tempf=\"","");
+	                    line = line.replace("</span>&#176;?!</p><div class=\"remarkContainer\">", "");
+	                    line = line.replaceFirst("\\d+", "");
+	                    line = line.replace("\">", "");
+	                    try {
+	                        location.setTemp(Integer.parseInt(line.trim()));
+	                        } catch (NumberFormatException e) {
+	                            e.printStackTrace();
+	                        }
+	                }
+	                if (line.contains("<p class=\"remark\">")) {
+	                    line = line.replace("<p class=\"remark\">","");
+	                    line = line.replace("</p>","");
+	                    line = line.trim();
+	                    location.setComment(line);
+	                }
+	                if (line.contains("</div><p class=\"flavor\">")) {
+                        line = line.replace("<</div><p class=\"flavor\">","");
+                        line = line.replace("</p>","");
+                        line = line.trim();
+                        location.setFlavour(line);
+                    }
+	            }
+//	            System.out.println(location.getLocationName());
+//	            System.out.println(location.getTemp());
+//	            System.out.println(location.getComment());
+//	            System.out.println();
+//	            System.out.println(location.toString());
+	            in.close();
+	        } catch (MalformedURLException e) {
+	        } catch (IOException e) {
+	        }
+	    }
 	
 	private static String getTimestamp() {
 	    Date date = new Date(System.currentTimeMillis());
@@ -159,7 +229,7 @@ public class MumbleIce implements Runnable{
         try {
             // lets connect to the murmurs ice endpoint
             ic = Ice.Util.initialize();
-            Ice.ObjectPrx base = ic.stringToProxy("Meta:tcp -h 127.0.0.1 -p 6502");
+            Ice.ObjectPrx base = ic.stringToProxy("Meta:tcp -h " + ip + " -p " + port);
             MetaPrx meta = MetaPrxHelper.checkedCast(base);
    
             // didnt work
@@ -183,21 +253,46 @@ public class MumbleIce implements Runnable{
                 for(Map.Entry<Integer, Channel> chanEntry : channels.entrySet()) {                  
                     Channel chan = chanEntry.getValue();
                     
-                    // look for channel with name "..."
-                    if(chan.name.endsWith(topchannel)) {
-                        // get all sub channels and loop
-                        ArrayList<Integer> subChans = getSubChannelsOfID(serv, chan.id);       
-                        for(Integer i : subChans) {
-                            // get subchan and work with it
-                            Channel subChan = getChanByID(serv, i);
-                            if(subChan != null) {
-                                String playername = subChan.name.split("\\s+")[0];
-                                Player player = new Player(playername);
-                                parseOfficialStats(player);
-                                subChan.name = player.officialString();
-                                subChan.description = "last updated: " + getTimestamp();
-                                serv.setChannelState(subChan);
-                          }
+                    
+                    if (lol) {
+                        if(chan.name.endsWith(lolTopchannel)) {
+                            // get all sub channels and loop
+                            ArrayList<Integer> subChans = getSubChannelsOfID(serv, chan.id);       
+                            for(Integer i : subChans) {
+                                // get subchan and work with it
+                                Channel subChan = getChanByID(serv, i);
+                                if(subChan != null) {
+                                    String playername = subChan.name.split("\\s+")[0];
+                                    Player player = new Player(playername);
+                                    parseOfficialStats(player);
+                                    subChan.name = player.officialString();
+                                    subChan.description = "last updated: " + getTimestamp();
+                                    serv.setChannelState(subChan);
+                              }
+                            }
+                        }
+                    }
+                    if (weather) {
+                        if (chan.name.endsWith(wetterTopchannel)) {
+                            ArrayList<Integer> subChans = getSubChannelsOfID(serv, chan.id);       
+                            for(Integer i : subChans) {
+                                Channel subChan = getChanByID(serv, i);
+                                if(subChan != null) {
+                                    String plz = subChan.name.split("\\s+")[0];
+                                    Location location = null;
+                                    try {
+                                        location = new Location(plz);
+                                    } catch (Exception e) {
+                                        System.err.println("Integer parse Error");
+                                    }
+                                    if (location != null) {
+                                        getWeather(location);
+                                        subChan.name = location.toString();
+                                        subChan.description = location.getFlavour();
+                                        serv.setChannelState(subChan);
+                                    }
+                              }
+                            }
                         }
                     }
                 }
